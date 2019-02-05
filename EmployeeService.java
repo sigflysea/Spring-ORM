7865748f5992b76270f154dd4df4c8df11f879ea
sigflysea <junfree@gmail.com>
2019-02-05 14:39:49 -0600

    Create EmployeeService.java

---
+++ EmployeeService.java
@@ -0,0 +1,6 @@
+package com.infosys.service;
+import com.infosys.domain.Employee;
+public interface EmployeeService { 
+        public void insertEmployee(Employee emp); 
+        public int removeEmployee(int empId); 
+}
